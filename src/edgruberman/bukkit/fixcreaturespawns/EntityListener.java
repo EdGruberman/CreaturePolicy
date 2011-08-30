afe7c0b3410e2fac065807c351e12db109266213
EdGruberman <ed@rjump.com>
2011-08-29 21:30:29 -0700

    CHG: Added feature to specify data for unsafe materials CHG: Added ability to specify unsafe spawn reasons CHG: Updated configuration file management class to match standard FIX: Code organization

--- src/edgruberman/bukkit/fixcreaturespawns/EntityListener.java
+++ src/edgruberman/bukkit/fixcreaturespawns/EntityListener.java
@@ -1,14 +1,17 @@
 package edgruberman.bukkit.fixcreaturespawns;
 
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.material.MaterialData;
 
 import edgruberman.bukkit.messagemanager.MessageLevel;
+import edgruberman.bukkit.messagemanager.channels.Channel;
 
-public class EntityListener extends org.bukkit.event.entity.EntityListener {
+class EntityListener extends org.bukkit.event.entity.EntityListener {
     
-    public EntityListener(Main plugin) {
+    EntityListener(Main plugin) {
         plugin.getServer().getPluginManager().registerEvent(Event.Type.CREATURE_SPAWN, this, Event.Priority.Normal, plugin);
     }
     
@@ -18,15 +21,18 @@ public class EntityListener extends org.bukkit.event.entity.EntityListener {
         
         if (Main.isAllowedSpawn(event)) return;
         
-        Main.messageManager.log(
-                "Cancelling " + event.getCreatureType().getName()
-                    + " spawn in \"" + event.getLocation().getWorld().getName() + "\" at"
-                    + " x: " + event.getLocation().getBlockX()
-                    + " y: " + event.getLocation().getBlockY()
-                    + " z: " + event.getLocation().getBlockZ()
-                    + " on " + event.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().name()
-                , MessageLevel.FINER
-        );
+        if (Main.messageManager.isLevel(Channel.Type.LOG, MessageLevel.FINER)) {
+            Block block = event.getLocation().getBlock().getRelative(BlockFace.DOWN);
+            Main.messageManager.log(
+                    "Cancelling " + event.getCreatureType().getName()
+                        + " spawn in \"" + event.getLocation().getWorld().getName() + "\" at"
+                        + " x: " + event.getLocation().getBlockX()
+                        + " y: " + event.getLocation().getBlockY()
+                        + " z: " + event.getLocation().getBlockZ()
+                        + " on " + new MaterialData(block.getType(), block.getData())
+                    , MessageLevel.FINER
+            );
+        }
         event.setCancelled(true);
     }
 }
--- src/edgruberman/bukkit/fixcreaturespawns/Main.java
+++ src/edgruberman/bukkit/fixcreaturespawns/Main.java
@@ -1,43 +1,57 @@
 package edgruberman.bukkit.fixcreaturespawns;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.material.MaterialData;
+import org.bukkit.util.config.Configuration;
 
 import edgruberman.bukkit.messagemanager.MessageLevel;
 import edgruberman.bukkit.messagemanager.MessageManager;
 
 public class Main extends org.bukkit.plugin.java.JavaPlugin {
+    
+    private static final ArrayList<String> DEFAULT_UNSAFE_SPAWN_REASONS = new ArrayList<String>(Arrays.asList(
+              "NATURAL"
+    ));
+    private static final ArrayList<String> DEFAULT_UNSAFE_CREATURE_TYPES = new ArrayList<String>(Arrays.asList(
+              "CREEPER"
+            , "SKELETON"
+            , "SPIDER"
+            , "ZOMBIE"
+    ));
+    private static final ArrayList<String> DEFAULT_SAFE_MATERIAL = new ArrayList<String>(Arrays.asList(
+              "LEAVES"
+    ));
+    private static final int DEFAULT_SAFE_RADIUS = 24;
 
     static ConfigurationFile configurationFile;
     static MessageManager messageManager;
     
-    private static List<String> unsafeCreatureTypes = new ArrayList<String>();
-    private static List<String> safeMaterial = new ArrayList<String>();
-    private static int safeRadiusSquared = 24;
+    private static Set<SpawnReason> unsafeSpawnReasons = new HashSet<SpawnReason>();
+    private static Set<CreatureType> unsafeCreatureTypes = new HashSet<CreatureType>();
+    private static Set<Material> safeMaterial = new HashSet<Material>();
+    private static Set<MaterialData> safeMaterialData = new HashSet<MaterialData>();
+    private static int safeRadiusSquared = -1;
     
     public void onLoad() {
-        Main.configurationFile = new ConfigurationFile(this);
-        Main.configurationFile.load();
-        
         Main.messageManager = new MessageManager(this);
         Main.messageManager.log("Version " + this.getDescription().getVersion());
+        
+        Main.configurationFile = new ConfigurationFile(this);
     }
 	
     public void onEnable() {
-        Main.unsafeCreatureTypes = this.getConfiguration().getStringList("unsafeCreatureTypes", Main.unsafeCreatureTypes);
-        Main.messageManager.log("Unsafe Creature Types: " + Main.unsafeCreatureTypes, MessageLevel.CONFIG);
-        
-        Main.safeMaterial = this.getConfiguration().getStringList("safeMaterial", Main.safeMaterial);
-        Main.messageManager.log("Safe Material: " + Main.safeMaterial, MessageLevel.CONFIG);
-        
-        Main.safeRadiusSquared = (int) Math.pow(this.getConfiguration().getInt("safeRadius", (int) Math.sqrt(Main.safeRadiusSquared)), 2);
-        Main.messageManager.log("Safe Radius: " + (int) Math.sqrt(Main.safeRadiusSquared), MessageLevel.CONFIG);
-        
+        Main.loadConfiguration();
         new EntityListener(this);
         
         Main.messageManager.log("Plugin Enabled");
@@ -47,26 +61,67 @@ public class Main extends org.bukkit.plugin.java.JavaPlugin {
         Main.messageManager.log("Plugin Disabled");
     }
     
-    static boolean isAllowedSpawn(CreatureSpawnEvent event) {
-        // Only prevent unsafe creatures.
-        if (!unsafeCreatureTypes.contains(event.getCreatureType().getName())) return true;
+    static void loadConfiguration() {
+        Configuration cfg = Main.configurationFile.getConfiguration();
         
-        // Prevent on all safe materials.
-        if (safeMaterial.contains(event.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().name())) return false;
-        
-        if (event.getSpawnReason() != SpawnReason.NATURAL) return true;
-        
-        // Prevent if distance from player is within the safety radius.
-        for (Player player : event.getLocation().getWorld().getPlayers()) {
-            double distanceSquared = player.getLocation().distanceSquared(event.getLocation());
+        Main.unsafeSpawnReasons.clear();
+        for (String name : cfg.getStringList("unsafeSpawnReasons", Main.DEFAULT_UNSAFE_SPAWN_REASONS))
+            Main.unsafeSpawnReasons.add(SpawnReason.valueOf(name));
             
-            if (distanceSquared <= Main.safeRadiusSquared) {
-                Main.messageManager.log("Unsafe " + event.getSpawnReason() + " spawn at " + (int) Math.sqrt(distanceSquared) + "m near " + player.getName(), MessageLevel.FINE);
-                return false;
+        Main.messageManager.log("Unsafe Spawn Reasons: " + Main.unsafeSpawnReasons.toString(), MessageLevel.CONFIG);
+        
+        Main.unsafeCreatureTypes.clear();
+        for (String name : cfg.getStringList("unsafeCreatureTypes", Main.DEFAULT_UNSAFE_CREATURE_TYPES))
+            Main.unsafeCreatureTypes.add(CreatureType.valueOf(name));
+            
+        Main.messageManager.log("Unsafe Creature Types: " + Main.unsafeCreatureTypes.toString(), MessageLevel.CONFIG);
+        
+        Main.safeMaterial.clear();
+        Main.safeMaterialData.clear();
+        for (String entry : cfg.getStringList("safeMaterial", Main.DEFAULT_SAFE_MATERIAL)) {
+            Material material = null;
+            Byte data = null;
+            if (entry.contains(":")) {
+                material = Material.valueOf(entry.split(":")[0]);
+                data = Byte.parseByte(entry.split(":")[1]);
+                Main.safeMaterialData.add(new MaterialData(material, data));
+            } else {
+                material = Material.valueOf(entry);
+                Main.safeMaterial.add(material);
+            }
+        }
+        Main.messageManager.log("Safe Material: " + Main.safeMaterial.toString() + " " + Main.safeMaterialData.toString(), MessageLevel.CONFIG);
+        
+        Main.safeRadiusSquared = (int) Math.pow(cfg.getInt("safeRadius", Main.DEFAULT_SAFE_RADIUS), 2);
+        int safeRadius = Main.safeRadiusSquared;
+        if (Main.safeRadiusSquared >= 0) safeRadius = (int) Math.sqrt(Main.safeRadiusSquared);
+        Main.messageManager.log("Safe Radius: " + safeRadius, MessageLevel.CONFIG);
+    }
+    
+    static boolean isAllowedSpawn(CreatureSpawnEvent event) {
+        // Allow safe spawn reasons.
+        if (!Main.unsafeSpawnReasons.contains(event.getSpawnReason())) return true;
+        
+        // Allow safe creatures.
+        if (!Main.unsafeCreatureTypes.contains(event.getCreatureType())) return true;
+        
+        // Prevent on safe materials.
+        Block block = event.getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if (Main.safeMaterial.contains(block.getType())) return false;
+        if (Main.safeMaterialData.contains(new MaterialData(block.getType(), block.getData()))) return false;
+        
+        // Prevent if distance from player is within a defined safety radius.
+        if (Main.safeRadiusSquared >= 0) {
+            for (Player player : event.getLocation().getWorld().getPlayers()) {
+                double distanceSquared = player.getLocation().distanceSquared(event.getLocation());
+                if (distanceSquared <= Main.safeRadiusSquared) {
+                    Main.messageManager.log("Unsafe " + event.getSpawnReason() + " spawn at " + (int) Math.sqrt(distanceSquared) + "m near " + player.getName(), MessageLevel.FINE);
+                    return false;
+                }
             }
         }
         
-        // Passed all the checks, allow spawn by default.
+        // Passed all the checks, allow spawn.
         return true;
     }
 }
